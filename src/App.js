f357d0639986d0d2b19089ab408d2c73cea0ff7b
David Mullally <mullallydavid7@gmail.com>
2023-01-27 16:48:40 +0100

    "refactored codee for conditional rendering (login/logout)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "./context/auth.context";
 ///////Our Components///////////
 ///Components/////
 import Navbar from "./components/Navbar";
@@ -17,19 +18,54 @@ import ResourceDetailsPage from "./pages/ResourceDetailsPage";
 import ProfileEditPage from "./pages/ProfileEditPage"
 import CreatePostPage from "./pages/CreatePostPage";
 import EditResourcePage from "./pages/EditResourcePage";
+import axios from "axios";
 /////////////////////////////////
 
 function App() {
+  const baseURL = process.env.REACT_APP_API_URL;
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+  const { storedToken, authenticateUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [profile, setProfile] = useState([]);
+  const getUserProfile = () => {
+      isLoggedIn && axios
+        .get(`${baseURL}/api/users/${user._id}`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        })
+        .then((response) => {
+          const oneProfile = response.data;
+          setProfile(oneProfile)
+        })
+        .catch((error) => console.log(error));
+    };
+
+    useEffect(() => {
+      getUserProfile();
+    }, [user]);
+  const deleteUser = () => {
+    const storedToken = localStorage.getItem("authToken");
+
+    axios
+      .delete(`${baseURL}/api/users/${user._id}`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then(() => {
+        navigate("/");
+        logOutUser();
+      })
+      .catch((err) => console.log(err));
+  };
+  console.log(profile)
+
 
   return (
     <div className="App">
-      <Navbar />
+      <Navbar profileImage={profile.profileImage} />
       <Routes>
-        <Route path={"/"} element={<HomePage />} />
+        <Route path={"/"} element={<ProfilePage profile={profile} callbackToDeleteUser={deleteUser} />}  />
         <Route path={"/signup"} element={<SignUpPage />} />
         <Route path={"/logIn"} element={<LogInPage />} />
-
-        <Route path={`/profiles/:userId`} element={<ProfilePage />} />
+        <Route path={`/profiles/:userId`} element={<ProfilePage profile={profile} callbackToDeleteUser={deleteUser} />} />
         <Route path={"/profiles/:userId/edit"} element={<ProfileEditPage />} />
         <Route path={"/resources"} element={<ResourceListPage />} />
         <Route path={"/resources/:resourceId"} element={<ResourceDetailsPage />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,16 @@
 import "../components/Navbar.css";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 import logo from "../data/daddabase-logo.png"
 import { AuthContext } from "../context/auth.context";
 import { useContext, useEffect, useState} from 'react';
 import { ThemeContext } from './../context/theme.context';
 import axios from "axios";
+import { propTypes } from "react-bootstrap/esm/Image";
 const baseURL = process.env.REACT_APP_API_URL;
 
-function Navbar() {
-  const [profile, setProfile] = useState([]);
+function Navbar(props) {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   const value = useContext(ThemeContext);
- 
-
-  useEffect(()=>{
-    axios.get(`${baseURL}/api/users/${user._id}`)
-      .then((res) => {
-        setProfile(res.data)
-      })
-      .catch((error) => console.log(error))
-  }, [])
-
-  console.log(profile.profileImage);
 
   return (
     <div className={`Navbar ${value}`}>
@@ -32,7 +21,7 @@ function Navbar() {
           <span> we've got your back!</span>
         </div>
       </div>
-
+      <div className>
       {isLoggedIn && (
         <div className="navlinks">
           <NavLink className="NavLink" to="/">
@@ -47,14 +36,13 @@ function Navbar() {
           <NavLink className="NavLink" to="/posts">
             Posts
           </NavLink>
-          <button className="logout-button" onClick={logOutUser}>
+          <div className="Navbar-user">
+          <img src={props.profileImage} alt="" />
+            <span>{`${user && user.name}`}</span> 
+            <button className="logout-button" onClick={logOutUser}>
             Log Out
           </button>
-          <h5>{`Account Owner: ${user && user.name}`}</h5>
-          {profile.profileImage &&
-            <img src={profile.profileImage} alt=" "/>
-          }
-          
+          </div>
         </div>
       )}
 
@@ -68,6 +56,7 @@ function Navbar() {
           </NavLink>
         </div>
       )}
+      </div>
     </div>
   );
 }
