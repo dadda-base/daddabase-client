e49ad08f74cb6d9803ba91c2b9476833119114ff
Lai In Chang <lavender880331@gmail.com>
2023-01-27 15:22:29 +0100

    some problem happen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
-import "../components/Navbar.css";
+// import "../components/Navbar.css";
 import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../data/daddabase-logo.png"
 import { AuthContext } from "../context/auth.context";
-import { useContext, useEffect, useState} from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from './../context/theme.context';
 import axios from "axios";
+import { Container } from "react-bootstrap";
 const baseURL = process.env.REACT_APP_API_URL;
 
 function Navbar() {
   const [profile, setProfile] = useState([]);
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   const value = useContext(ThemeContext);
- 
 
-  useEffect(()=>{
+
+  useEffect(() => {
     axios.get(`${baseURL}/api/users/${user._id}`)
       .then((res) => {
         setProfile(res.data)
@@ -21,17 +22,62 @@ function Navbar() {
       .catch((error) => console.log(error))
   }, [])
 
-  console.log(profile.profileImage);
+
 
   return (
+
+    // <Navbar sticky="top" bg="primary" variant="dark">
+    //   <Container>
+    //   <Navbar.Brand href="/">
+    //   <img className={"Navbar-logo"} src={logo} alt="not working" />
+    //       <div>
+    //         <h1 className="Navbar-brandname">DaddaBase</h1>
+    //         <span> we've got your back!</span>
+    //       </div>
+    //   </Navbar.Brand>
+    
+
+
+    //     <div className="navlinks">
+    //       <NavLink className="NavLink" to="/">
+    //         Home
+    //       </NavLink>
+    //       <NavLink className="NavLink" to={`/profiles/${user._id}`}>
+    //         Profile
+    //       </NavLink>
+    //       <NavLink className="NavLink" to="/resources">
+    //         Resources
+    //       </NavLink>
+    //       <NavLink className="NavLink" to="/posts">
+    //         Posts
+    //       </NavLink>
+    //       <button className="logout-button" onClick={logOutUser}>
+    //         Log Out
+    //       </button>
+    //       <h5>{`Account Owner: ${user && user.name}`}</h5>
+    //       {profile.profileImage &&
+    //         <img src={profile.profileImage} alt=" " />
+    //       }
+    //     </div>
+
+    //     {!isLoggedIn && (
+    //       <div className="navlinks">
+    //         <NavLink className="NavLink" to="/login">
+    //           Log In
+    //         </NavLink>
+    //         <NavLink className="NavLink" to="/signUp">
+    //           Sign Up
+    //         </NavLink>
+    //       </div>
+    //     )}
+    //   </Container>
+    // </Navbar>
+
+
+
+
     <div className={`Navbar ${value}`}>
-      <div className="Navbar-brand">
-        <img className={"Navbar-logo"} src={logo} alt="not working" />
-        <div>
-          <h1 className="Navbar-brandname">DaddaBase</h1>
-          <span> we've got your back!</span>
-        </div>
-      </div>
+      
 
       {isLoggedIn && (
         <div className="navlinks">
@@ -52,9 +98,8 @@ function Navbar() {
           </button>
           <h5>{`Account Owner: ${user && user.name}`}</h5>
           {profile.profileImage &&
-            <img src={profile.profileImage} alt=" "/>
+            <img src={profile.profileImage} alt=" " />
           }
-          
         </div>
       )}
 
@@ -68,7 +113,7 @@ function Navbar() {
           </NavLink>
         </div>
       )}
-    </div>
+    </div> 
   );
 }
 
