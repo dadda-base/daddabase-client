4f0bcf531e58f9b0808504c05079eb454e3df0c1
Lai In Chang <lavender880331@gmail.com>
2023-01-31 19:16:34 +0100

    finish css of profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,67 +1,97 @@
 import "../pages/ProfilePage.css";
-import { useContext, useEffect, useState} from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/auth.context";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 import axios from 'axios'
+import { LinkContainer } from "react-router-bootstrap";
 const baseURL = process.env.REACT_APP_API_URL;
 
 function ProfilePage(props) {
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+  const { isLoggedIn, user } = useContext(AuthContext);
   const { storedToken, authenticateUser } = useContext(AuthContext);
   const [profile, setProfile] = useState([])
   console.log(profile)
-    const getProfile = () => {
-      axios.get(baseURL + "/api/users/" + user._id, {
-        headers: { Authorization: `Bearer ${storedToken}` },
+  const getProfile = () => {
+    axios.get(baseURL + "/api/users/" + user._id, {
+      headers: { Authorization: `Bearer ${storedToken}` },
+    })
+      .then((res) => {
+        setProfile(res.data)
       })
-        .then((res) => {
-          setProfile(res.data)
-        })
-        .catch((error) => console.log(error));
-    }
-  
-    useEffect(() => {
-      getProfile();
-    }, [])
-  
+      .catch((error) => console.log(error));
+  }
+
+  useEffect(() => {
+    getProfile();
+  }, [])
+
 
 
   return (
-    <>
-      <div className="ProfilePage">
+    <div className="ProfilePage">
+
+
+      <Card style={{ width: '18rem' }} className="profileCard">
         {isLoggedIn && (
           <>
-            <img className="profile-image" src={profile.profileImage} alt="" />
-            <h1>{profile.username}</h1>
+            <Card.Img variant="top" src={profile.profileImage} alt="" />
 
-            <Link to={`/profiles/${profile._id}/edit`}> Edit Profile</Link>
-            <button onClick={props.callbackToDeleteUser}> Delete Profile</button>
+            <Card.Body>
+              <Card.Title>User Name: {profile.username}</Card.Title>
+              <Card.Text>Name: {profile.name}</Card.Text>
+              <Card.Text>Email: {profile.email}</Card.Text>
+
+              <LinkContainer to={`/profiles/${profile._id}/edit`}>
+                <Button variant="primary">Edit Profile</Button>
+              </LinkContainer>
+              <Button variant="danger" onClick={props.callbackToDeleteUser}>Delete Profile</Button>
+            </Card.Body>
           </>
         )}
-        {profile.resources &&
-          profile.resources.map((resource) => {
-            return (
-              <div className="ResourceCard card">
-                <Link to={`/resources/${resource._id}`}>
-                  <h3>{resource.title}</h3>
-                </Link>
-              </div>
-            )
-          })
-        }
-        {profile.posts &&
-          profile.posts.map((post) => {
-            return (
-              <div className="post">
-                <h1>Post Title:{post.title}</h1>
-                <h4>Post description:{post.description}</h4>
-              </div>
-            );
-          })
-        }
+      </Card>
 
+      <div className="listContainer">
+        <ListGroup as="ul">
+          <ListGroup.Item as="li" active>
+            <h4>My Resources</h4>
+          </ListGroup.Item>
+
+          {profile.resources &&
+            profile.resources.map((resource) => {
+              return (
+                <ListGroup.Item as="li" className="profileListBar">
+                  <Link to={`/resources/${resource._id}`} className="profileList">
+                    <h5>Title: {resource.title}</h5>
+                  </Link>
+                </ListGroup.Item>
+              )
+            })
+          }
+        </ListGroup>
+
+        <ListGroup as="ul" className="listGroup">
+          <ListGroup.Item as="li" variant="success">
+            <h4>My Posts</h4>
+          </ListGroup.Item>
+
+          {profile.posts &&
+            profile.posts.map((post) => {
+              return (
+                <ListGroup.Item as="li">
+
+                  <h5>Title: {post.title}</h5>
+                  <h5>Description: {post.description}</h5>
+                </ListGroup.Item>
+              )
+            })
+          }
+        </ListGroup>
       </div>
-    </>
+    </div>
+
   );
 }
 
