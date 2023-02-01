d22cf0681886973e8ef5c1c007a012838f8957ba
Lai In Chang <lavender880331@gmail.com>
2023-02-01 06:52:36 +0100

    finish profile and edit profile css

--- src/pages/ProfileEditPage.js
+++ src/pages/ProfileEditPage.js
@@ -1,21 +1,39 @@
 import "../pages/ProfileEditPage.css";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/auth.context";
 import { useNavigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import axios from "axios";
 
 function ProfileEditPage() {
-  const { isLoggedIn, user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const baseURL = process.env.REACT_APP_API_URL;
   const navigate = useNavigate();
+  const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const [profileImage, setProfileImage] = useState("");
+  const storedToken = localStorage.getItem("authToken");
 
-  const handleFormSubmit = (e) => {
+  useEffect(() => {
+    axios
+        .get(`${baseURL}/api/users/${user._id}`,
+        { headers: { Authorization: `Bearer ${storedToken}` } })
+        .then((response) => {
+            const user = response.data;
+            setName(user.name);
+            setUsername(user.username);
+            setProfileImage(user.profileImage);
+        })
+        .catch((error) => console.log(error));
+}, [user._id]);
+
+
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    const requestBody = { username, profileImage };
-    const storedToken = localStorage.getItem("authToken");
+    const requestBody = { name, username, profileImage };
 
     axios
       .put(`${baseURL}/api/users/${user._id}`, requestBody, {
@@ -23,38 +41,52 @@ function ProfileEditPage() {
       })
       .then((response) => {
         navigate(`/profiles/${user._id}`);
-        console.log(`new ${username}, ${profileImage} user: ${user._id}`)
+        console.log(`new ${name}  ${username}, ${profileImage} user: ${user._id}`)
       })
       .catch((error) => console.log(error));
   };
 
   return (
-    <div className="ProfileEditPage">
-      {isLoggedIn && (
-        <>
-          <h1>{user.name}</h1>
-          <h1>{user.email}</h1>
-          <form onSubmit={handleFormSubmit}>
-            <input
-              type="text"
-              name="username"
-              value={user.username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="choose a username"
-            />
+    <Form onSubmit={handleSubmit} className="editProfile">
+            <h3>Edit the Profile</h3>
+            <Container className="editProfileContainer">
+                <Form.Group className="mb-3" controlId="ControlInput1">
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Enter new name" />
+                </Form.Group>
 
-            <input
-              name="profileImage"
-              value={user.profileImage}
-              onChange={(e) => setProfileImage(e.target.value)}
-              placeholder="enter an image url"
-            />
+                <Form.Group className="mb-3" controlId="ControlInput1">
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Enter new username" />
+                </Form.Group>
 
-            <input type="submit" value="Submit" />
-          </form>
-        </>
-      )}
-    </div>
+                <Form.Group className="mb-3" controlId="ControlInput1">
+                    <Form.Label>Profile Image:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="profileImage"
+                        value={profileImage}
+                        onChange={(e) => setProfileImage(e.target.value)}
+                        placeholder="Enter new profile image url" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Container>
+        </Form>
+
+    
   );
 }
 
