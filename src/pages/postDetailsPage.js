d392667956d0c111bf3dd88ab29246db1d6c6f49
Lai In Chang <lavender880331@gmail.com>
2023-02-02 12:03:00 +0100

    now can reder reply, but profile is not updated

--- src/pages/postDetailsPage.js
+++ src/pages/postDetailsPage.js
@@ -1,10 +1,9 @@
 import "../pages/PostDetailsPage.css";
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { Form, Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+
 
 import {
   MDBBtn,
@@ -14,12 +13,11 @@ import {
   MDBCardImage,
   MDBCol,
   MDBContainer,
-  MDBIcon,
   MDBRow,
   MDBTextArea,
 } from "mdb-react-ui-kit";
 import { LinkContainer } from "react-router-bootstrap";
-import { Nav } from "react-bootstrap";
+import { Container, Nav } from "react-bootstrap";
 
 const baseURL = process.env.REACT_APP_API_URL;
 
@@ -75,20 +73,23 @@ function PostDetailsPage() {
       .post(baseURL + "/api/posts/" + postId, requestBody,
         { headers: { Authorization: `Bearer ${storedToken}` } })
       .then((response) => {
+        setContent("")
+        setProfile("")
+
         navigate("/posts/" + postId)
       });
   };
 
 
   return (
-    <section className="vh-100" style={{ backgroundColor: "#eee" }}>
+    <section className="100%" style={{ backgroundColor: "#eee" }}>
       {post &&
-     
-        <MDBContainer className="py-5" style={{ maxWidth: "1000px" }}  >
+
+        <MDBContainer className="py-5" style={{ maxWidth: "800px" }} onSubmit={handleSubmit} >
           <MDBRow className="justify-content-center">
             <MDBCol md="12" lg="10" xl="8">
               <MDBCard>
-                <MDBCardBody>
+                <MDBCardBody className="topic">
                   <div className="d-flex flex-start align-items-center">
                     <MDBCardImage
                       className="rounded-circle shadow-1-strong me-3"
@@ -98,7 +99,7 @@ function PostDetailsPage() {
                       height="60"
                     />
                     <div>
-                      <h6 className="fw-bold text-primary mb-1">{post.user.username}</h6>
+                      <h4 className="fw-bold text-primary mb-1">{post.user.username}</h4>
                       <p className="text-muted small mb-0">
                         {/* Shared publicly - Jan 2020 */}
                       </p>
@@ -107,52 +108,91 @@ function PostDetailsPage() {
                   <h3 className="mt-3 mb-4 pb-2">{post.title}</h3>
                   <p className="mt-3 mb-4 pb-2" style={{ textAlign: "start" }}>
                     {post.description}
-                  </p>                  
+                  </p>
                 </MDBCardBody>
-
-                <MDBCardFooter
-                  className="py-3 border-0"
-                  style={{ backgroundColor: "#f8f9fa" }}
-
-                >
-                  <div className="d-flex flex-start w-100">
+                
+                
+                {post.comment
+                ?
+                post.comment.map((e) => 
+                  
+                <MDBCardBody>
+                  <hr />
+                  
+                  <Container className="commentContainer">
+                    <div className="commentUser">
                     <MDBCardImage
                       className="rounded-circle shadow-1-strong me-3"
-                      src={profile.profileImage}
+                      src={e.profileImage}
                       alt="avatar"
                       width="40"
                       height="40"
                     />
-                    <MDBTextArea
-                      type="text"
-                      name="content"
-                      value={content}
-                      onChange={(e) => setContent(e.target.value)}
-                      placeholder="leave your comment"
-                      label='Message'
-                      id='content'
-                      rows={4}
-                      style={{ backgroundColor: '#fff' }}
-                      wrapperClass="w-100" />
-                  </div>
-                  <div className="float-end mt-2 pt-1">
-                    <MDBBtn size="sm" className="me-1" type="submit" >
-                      Post comment
-                    </MDBBtn>
+                    
+                      <h6 className="fw-bold text-primary mb-1">{e.username}</h6>
+                      <p className="text-muted small mb-0">
+                        {/* Shared publicly - Jan 2020 */}
+                      </p>
+                    </div>
+                  
+                  
+                  <p className="mt-3 mb-4 pb-2" style={{ textAlign: "start" }}>
+                    {e.content}
+                  </p>
+                  </Container>
+                  
+                </MDBCardBody>
 
-                    <MDBBtn outline size="sm">
-                      <LinkContainer to="/posts">
-                        <Nav.Link>Cancel</Nav.Link>
-                      </LinkContainer>
-                    </MDBBtn>
-                  </div>
-                </MDBCardFooter>
+                  )
+                :<></>
+                 
                 
+                }
+
+                <form onSubmit={handleSubmit}>
+                  <MDBCardFooter
+                    className="py-3 border-0"
+                    style={{ backgroundColor: "#f8f9fa" }}
+
+                  >
+                    <div className="d-flex flex-start w-100">
+                      <MDBCardImage
+                        className="rounded-circle shadow-1-strong me-3"
+                        src={profile.profileImage}
+                        alt="avatar"
+                        width="40"
+                        height="40"
+                      />
+                      <MDBTextArea
+                        type="text"
+                        name="content"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        placeholder="leave your comment"
+                        label='Message'
+                        id='content'
+                        rows={4}
+                        style={{ backgroundColor: '#fff' }}
+                        wrapperClass="w-100" />
+                    </div>
+                    <div className="float-end mt-2 pt-1">
+                      <MDBBtn size="sm" className="me-1" type="submit" >
+                        Post comment
+                      </MDBBtn>
+
+                      <MDBBtn outline size="sm">
+                        <LinkContainer to="/posts">
+                          <Nav.Link>Cancel</Nav.Link>
+                        </LinkContainer>
+                      </MDBBtn>
+                    </div>
+                  </MDBCardFooter>
+                </form>
               </MDBCard>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
-       
+
       }
     </section>
   );
